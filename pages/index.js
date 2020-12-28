2375cd039214547fde00a2fa9fa3012a5283eee5
Nate Davis Olds <nate@davisolds.com>
2020-12-28 17:48:14 -0500

    Initial content commit

--- pages/index.js
+++ pages/index.js
@@ -5,61 +5,25 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Exploring Code - davisolds.com</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>An invitation to explore code.</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <article className={styles.article}>
+          <p>If anyone tells you there is a “right”  syntax or format to write web applications, they haven’t been programming long enough.  There are countless techniques and styles that people have adopted as <em>the way</em> to code and while it works for the them in their niche domain space, it will not universally fit every problem.  As coders, the closest we can get to the one way is to have practices and patterns that are mutually agreed upon within a team to solve that domain problem.  But to decide on the right practice or pattern, we first need to explore and share the challenges and advantages of each approach.</p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <p>In this series, I aim to explore various ways to accomplish the same problem.  Every segment, the problem space will be viewed with a different lens resulting in different decisions and results.  The end code will work for that case but also be available to compare and contracts its strengths with other approaches.</p>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <p>Through this exploration, I aim to gain a more robust understanding of the possible ways to solve a problem and be able to articulate and then choose the right pattern for the next problem that I am trying to solve.  By documenting this exploration, there will be a repository of reference material to support decisions for the practices and patterns agreed upon within a team.</p>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          <p>I invite you to join me.  Through deep analysis and discussion, I believe the approaches (and then the decisions) will be sharpened to its distinguishing point.  And by knowing the distinguishing point, we will make better choices when writing code.  With better choices, comes cleaner code that is developed quicker and is easier to maintain long term.</p>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <p>Let’s get started.</p>
+        </article>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
