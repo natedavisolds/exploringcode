6f6a236d6eb11e4b8c2f3bacd17d6af6c6ca306d
Nate Davis Olds <nate@davisolds.com>
2020-12-31 11:56:15 -0500

    Removing server side scripts to see if it fix deploy bug

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,6 +1,4 @@
-import matter from 'gray-matter'
 import ReactMarkdown from 'react-markdown'
-import {getAllPosts} from '../../lib/posts'
 import Link from 'next/link'
 
 const Related = ({title="Related Posts",posts=[]}) =>
@@ -14,7 +12,7 @@ const Related = ({title="Related Posts",posts=[]}) =>
   </section>
 
 
-export default function Post({frontmatter,markdownBody,allPosts}) {
+export default function Post({frontmatter={title:''},markdownBody="",allPosts=[]}) {
   return (
     <main className="main">
       <h1 className="title">{frontmatter.title}</h1>
@@ -25,40 +23,4 @@ export default function Post({frontmatter,markdownBody,allPosts}) {
       </article>
     </main>
   )
-}
-
-export async function getStaticProps({ ...ctx }) {
-  const { slug } = ctx.params
-
-  const content = await import(`../../data/posts/${slug}.md`)
-  const config = await import(`../../data/site.json`)
-  const data = matter(content.default)
-
-  return {
-    props: {
-      siteTitle: config.title,
-      frontmatter: data.data,
-      markdownBody: data.content,
-      allPosts: getAllPosts(['title','slug'])
-    },
-  }
-}
-
-export async function getStaticPaths() {
-  const blogSlugs = ((context) => {
-    const keys = context.keys()
-    const data = keys.map((key, index) => {
-      let slug = key.replace(/^.*[\\\/]/, '').slice(0, -3)
-
-      return slug
-    })
-    return data
-  })(require.context('../../data/posts', true, /\.md$/))
-
-  const paths = blogSlugs.map((slug) => `/posts/${slug}`)
-
-  return {
-    paths,
-    fallback: false,
-  }
 }
